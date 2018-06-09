314b51d21c231273511d3ef87d8c0d93c6d88a7f
masataka <ar.tec.masa@gmail.com>
2018-06-09 15:20:56 +0900

    all_black

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_many :addresses
-  accepts_nested_attributes_for :addresses, allow_destroy: true
+  accepts_nested_attributes_for :addresses, allow_destroy: true, reject_if: :all_blank
 
   validates :username,  presence: true
   validates :age,       presence: true
